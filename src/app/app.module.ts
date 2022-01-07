d9b6e3595a5da24ce37f4e4d6ab415600d6de386
unknown <anggaqr09@gmail.com>
2022-01-07 13:55:56 +0700

    Project-Akhir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DivisionComponent } from './division/division.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppMenuComponent,
+    DashboardComponent,
+    DivisionComponent
   ],
   imports: [
     BrowserModule,
